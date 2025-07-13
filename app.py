b968b178d861c2ae1f591f59c56634012afeadc7
diego <diegoandresburgess@gmail.com>
2025-07-13 09:55:58 -0500

    Created database and nonfunctional table on site. Also enabled prettier extension

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import timezone, datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # relative path 
+
+db = SQLAlchemy(app) # initializing database
+
+class Todo(db.Model):   # creating a model
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    date_created = db.Column(db.DateTime, default = datetime.now(timezone.utc)) # i would like this to print my local time, not sure if it does that
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 
